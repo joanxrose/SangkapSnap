134c62a325c3e3d0f44dce1a2f8f58da051e798d
joanxrose <jcpaelden@up.edu.ph>
2024-04-19 22:16:57 +0800

    chore: add initial assets and files

---
+++ main_app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:main_app/themes/customTheme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  final String appTitle = "Sangkap Snap App";
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: appTitle,
+        theme: customTheme,
+        home: const Scaffold(
+          body: Center(
+            child: Text(
+              "This is the Sangkap Snap app!",
+            ),
+          ),
+        ));
+  }
+}
---
+++ main_app/lib/themes/customTheme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData customTheme = ThemeData(
+    brightness: Brightness.light,
+    useMaterial3: true,
+    scaffoldBackgroundColor: const Color(0xfffbfcf6),
+
+    // Font Family (Font weights: 400, 600, 800)
+    fontFamily: 'PPPangramSans');

4658d8bdb5a1155311e82fd97945031e6bcf5126
joanxrose <jcpaelden@up.edu.ph>
2024-04-25 21:30:38 +0800

    ui: add loading indicator and show detected objs on the page itself

--- main_app/lib/screens/displayImagePage.dart
+++ main_app/lib/screens/displayImagePage.dart
@@ -27,6 +27,12 @@ class _CameraPageState extends State<CameraPage> {
   late ModelObjectDetection _objectModel;
   List<ResultObjectDetection?> objDetect = [];
 
+  // Keeps track of the app state (loading)
+  bool _isLoading = false;
+
+  // Store all the classNames of the detected objects
+  List<String?> detectedOjectsList = [];
+
   @override
   void initState() {
     super.initState();
@@ -34,7 +40,6 @@ class _CameraPageState extends State<CameraPage> {
   }
 
   // Load model
-  //load your model
   Future loadModel() async {
     String pathObjectDetectionModelYolov5 = "assets/models/yolov5s.torchscript";
     String labelPath = "assets/models/labels_coco.txt";
@@ -77,9 +82,6 @@ class _CameraPageState extends State<CameraPage> {
 
   @override
   Widget build(BuildContext context) {
-    // Store all the classNames of the detected objects
-    List<String?> detectedOjectsList;
-
     return Scaffold(
         appBar: AppBar(
           backgroundColor: AppColors.mainWhite,
@@ -97,83 +99,169 @@ class _CameraPageState extends State<CameraPage> {
                       fontWeight: FontWeight.w800,
                       fontSize: 20)),
         ),
-        body: Column(
-          children: [
-            // If an image is selected, display that image
-            // Otherwise, show "No image is selected"
-            Center(
-                child: widget.selectedImage != null
-                    ? Padding(
-                        padding: const EdgeInsets.only(top: 20, bottom: 30),
-                        child: Image.file(
-                          widget.selectedImage!,
-                          fit: BoxFit.contain,
-                          height: 400,
-                          width: 400,
-                        ),
-                      )
-                    : const Padding(
-                        padding: EdgeInsets.only(top: 100, bottom: 40),
-                        child: Column(
-                          children: [
-                            Icon(
-                              Icons.image_not_supported_rounded,
-                              color: AppColors.mainGreen,
-                              size: 50,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              // If an image is selected, display that image
+              // Otherwise, show "No image is selected"
+              Center(
+                  child: widget.selectedImage != null
+                      ? Padding(
+                          padding: const EdgeInsets.only(top: 20, bottom: 30),
+                          child: Image.file(
+                            widget.selectedImage!,
+                            fit: BoxFit.contain,
+                            height: 400,
+                            width: 400,
+                          ),
+                        )
+                      : const Padding(
+                          padding: EdgeInsets.only(top: 100, bottom: 40),
+                          child: Column(
+                            children: [
+                              Icon(
+                                Icons.image_not_supported_rounded,
+                                color: AppColors.mainGreen,
+                                size: 50,
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              Text(
+                                "No image selected.",
+                                style: TextStyle(fontSize: 18),
+                              )
+                            ],
+                          ),
+                        )),
+
+              // Display the list of detected ingredients / objects
+              detectedOjectsList.isEmpty
+                  ? const SizedBox(height: 5)
+                  : Column(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(left: 30, bottom: 10),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              "Detected ingredients",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w800,
+                                  color: AppColors.mainGreen),
                             ),
-                            SizedBox(
-                              height: 8,
-                            ),
-                            Text(
-                              "No image selected.",
-                              style: TextStyle(fontSize: 18),
-                            )
-                          ],
+                          ),
                         ),
-                      )),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 40, right: 20),
+                          child: ListView.builder(
+                              // scrollDirection: Axis.vertical,
+                              physics: const NeverScrollableScrollPhysics(),
+                              shrinkWrap: true,
+                              itemCount: detectedOjectsList.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                return ListTile(
+                                  title: Text(detectedOjectsList[index]!),
+                                  trailing: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      IconButton(
+                                        icon: const Icon(
+                                          Icons.edit,
+                                          color: Colors.grey,
+                                        ),
+                                        onPressed: () async {
+                                          print(
+                                              "Edit ingredient from detected list");
 
-            // Only Go Back button will be visible when no image is selected
-            (widget.selectedImage != null)
-                ? Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 30),
-                    child: CustomButton(
-                      onPressed: () async {
-                        await runObjectDetection();
-
-                        detectedOjectsList =
-                            objDetect.map((obj) => obj!.className).toList();
-
-                        // print(detectedOjectsList);
-
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => DetectedPage(
-                                  selectedImage: _image,
-                                  objDetect: detectedOjectsList)),
-                        );
-                      },
-                      text: "Detect ingredients",
-                      backgroundColor: AppColors.mainGreen,
-                      textColor: AppColors.mainWhite,
+                                          // Alert dialog
+                                        },
+                                      ),
+                                      IconButton(
+                                        icon: const Icon(
+                                          Icons.delete,
+                                          color: Colors.grey,
+                                        ),
+                                        onPressed: () {
+                                          print(
+                                              "Delete ingredient from detected list");
+                                          detectedOjectsList.remove(
+                                              detectedOjectsList[index]);
+                                          setState(() {
+                                            detectedOjectsList =
+                                                detectedOjectsList;
+                                          });
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }),
+                        ),
+                      ],
                     ),
-                  )
-                : const SizedBox(
-                    height: 5,
-                  ),
-            const SizedBox(
-              height: 15,
-            ),
-            Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30),
-                child: CustomButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  text: "Go back",
-                  backgroundColor: AppColors.mainWhite,
-                )),
-          ],
+
+              // Only Go Back button will be visible when no image is selected
+              (widget.selectedImage != null)
+                  ? Padding(
+                      padding:
+                          const EdgeInsets.only(left: 30, right: 30, top: 20),
+                      child: _isLoading == false
+                          ? CustomButton(
+                              onPressed: () async {
+                                setState(() {
+                                  _isLoading = true;
+                                });
+
+                                await runObjectDetection();
+
+                                setState(() {
+                                  _isLoading = false;
+
+                                  detectedOjectsList = objDetect
+                                      .map((obj) => obj!.className)
+                                      .toList();
+                                });
+
+                                // print(detectedOjectsList);
+
+                                /* Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => DetectedPage(
+                                    selectedImage: _image,
+                                    objDetect: detectedOjectsList)),
+                          ); */
+                              },
+                              text: "Detect ingredients",
+                              backgroundColor: AppColors.mainGreen,
+                              textColor: AppColors.mainWhite,
+                            )
+                          : const Center(
+                              child: CircularProgressIndicator(
+                                  valueColor: AlwaysStoppedAnimation<Color>(
+                                      AppColors.mainGreen)),
+                            ))
+                  : const SizedBox(
+                      height: 5,
+                    ),
+              const SizedBox(
+                height: 15,
+              ),
+
+              Padding(
+                  padding:
+                      const EdgeInsets.only(left: 30, right: 30, bottom: 50),
+                  child: CustomButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    text: "Go back",
+                    backgroundColor: AppColors.mainWhite,
+                  )),
+            ],
+          ),
         ));
   }
 }
--- main_app/lib/themes/colorConstants.dart
+++ main_app/lib/themes/colorConstants.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 class AppColors {
-  static const Color mainGreen = Color(0xff357960); // Example hex color
-  static const Color mainWhite = Color(0xfffbfcf6); // Example hex color
-  // Add more colors as needed
+  static const Color mainGreen = Color(0xff357960);
+  static const Color mainWhite = Color(0xfffbfcf6);
 }
